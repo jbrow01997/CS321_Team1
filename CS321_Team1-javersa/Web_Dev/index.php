aed9cbc2bfa051165aab6cebdafcfbed829d3dd5
javersa <javersa@gmu.edu>
2020-04-30 19:42:16 -0400

    Fixed index page.

--- CS321_Team1-javersa/Web_Dev/index.php
+++ CS321_Team1-javersa/Web_Dev/index.php
@@ -8,12 +8,8 @@
     </head>
 
     <body>
-		<?php $name = $_POST["name"]; ?>
-		<?php $gender = $_POST["gender"]; ?>
-		<?php $age = $_POST["age"]; ?>
-		<?php $dog = $_POST["dog"]; ?>
         <header>
-            <h1 class="logo"><a href="/Web_Dev/index.html">Agile Adoption</a></h1>
+            <h1 class="logo"><a href="/Web_Dev/index.php">Agile Adoption</a></h1>
             <nav">
                 <ul class="navList">
                     <li><a href="/Web_Dev/index.php">Home</a></li>
@@ -38,200 +34,167 @@
                  <!--Dogs wrapper-->
                  <section class="pets-wrapper">
                     <h2 class="section__title">Dogs</h2>
-
                     <div id="dogs" class="pets">
                         <!--javascript runs here-->
                     </div>
-					<script>
-						var name = "<?php echo $name;?>";
-						var gender = "<?php echo $gender;?>";
-						var breed = "<?php echo $name;?>";
-						var names = ["Bella","Charlie","Coco","Daisy","Lola","Molly","Phoebe","Poppy","Rosie","Teddy"];
-						var breeds = ["American Cattle Dog","American Pit Bull Terrier","American Shepherd","American Staffordshire Terrier","Border Collie","Boxer","Dachshund","German Shepherd","Rottweiler","Shih Tzu"];
-						
-						const Dogs = document.getElementById('dogs');
-						let dogCounter = 0;
-						
-						if (name.length === 0 || comparisons(name,names) === 0){
-							if (gender === "Both"){
-								if (breed.length === 0){
-									for (i = 0; i < names.length; i++) {
-										for (j = 0; j < breeds.length; j++){
-											img = '../Web_Dev/pet_images/' + names[i] + '/Female/' + breeds[j] + '/' + names[i] + '.jpg';
-											if (doesFileExist(img) == true){
-												Dogs.append(createDog(createNewPetElement(names[i]),names[i],'Female',breeds[j], img));
-											}
-											img = '../Web_Dev/pet_images/' + names[i] + '/Male/' + breeds[j] + '/' + names[i] + '.jpg';
-											if (doesFileExist(img) == true){
-												Dogs.append(createDog(createNewPetElement(names[i]),names[i],'Male',breeds[j], img));
-											}
-										}
-									}
-								}
-								else {
-									for (i = 0; i < names.length; i++) {
-										img = '../Web_Dev/pet_images/' + names[i] + '/Female/' + breed + '/' + names[i] + '.jpg';
-										if (doesFileExist(img) == true){
-											Dogs.append(createDog(createNewPetElement(names[i]),names[i],'Female',breed, img));
-										}
-										img = '../Web_Dev/pet_images/' + names[i] + '/Male/' + breed + '/' + names[i] + '.jpg';
-										if (doesFileExist(img) == true){
-											Dogs.append(createDog(createNewPetElement(names[i]),names[i],'Male',breed, img));
-										}
-									}
-								}
-							}
-							else {
-								if (breed.length === 0){
-									for (i = 0; i < names.length; i++) {
-										for (j = 0; j < breeds.length; j++){
-											img = '../Web_Dev/pet_images/' + names[i] + '/' + gender + '/' + breeds[j] + '/' + names[i] + '.jpg';
-											if (doesFileExist(img) == true){
-												Dogs.append(createDog(createNewPetElement(names[i]),names[i],gender,breeds[j], img));
-											}
-										}
-									}
-								}
-								else {
-									for (i = 0; i < names.length; i++) {
-										img = '../Web_Dev/pet_images/' + names[i] + '/' + gender + '/' + breed + '/' + names[i] + '.jpg';
-										if (doesFileExist(img) == true){
-											Dogs.append(createDog(createNewPetElement(names[i]),names[i],gender,breed, img));
-										}
-									}
-								}
-							}
-						}
-						else{
-							if (gender === "Both"){
-								if (breed.length === 0){
-									for (j = 0; j < breeds.length; j++){
-										img = '../Web_Dev/pet_images/' + name + '/Female/' + breeds[j] + '/' + name + '.jpg';
-										if (doesFileExist(img) == true){
-											Dogs.append(createDog(createNewPetElement(name),name,'Female',breeds[j], img));
-										}
-										img = '../Web_Dev/pet_images/' + name + '/Male/' + breeds[j] + '/' + name + '.jpg';
-										if (doesFileExist(img) == true){
-											Dogs.append(createDog(createNewPetElement(name),name,'Male',breeds[j], img));
-										}
-									}
-								}
-								else {
-									img = '../Web_Dev/pet_images/' + name + '/Female/' + breed + '/' + name + '.jpg';
-									if (doesFileExist(img) == true){
-										Dogs.append(createDog(createNewPetElement(name),name,'Female',breed, img));
-									}
-									img = '../Web_Dev/pet_images/' + name + '/Male/' + breed + '/' + name + '.jpg';
-									if (doesFileExist(img) == true){
-										Dogs.append(createDog(createNewPetElement(name),name,'Male',breed, img));
-									}
-								}
-							}
-							else {
-								if (breed.length === 0){
-									for (j = 0; j < breeds.length; j++){
-										img = '../Web_Dev/pet_images/' + name + '/' + gender + '/' + breeds[j] + '/' + name + '.jpg';
-										if (doesFileExist(img) == true){
-											Dogs.append(createDog(createNewPetElement(name),name,gender,breeds[j], img));
-										}
-									}
-								}
-								else {
-									img = '../Web_Dev/pet_images/' + name + '/' + gender + '/' + breed + '/' + name + '.jpg';
-									if (doesFileExist(img) == true){
-										Dogs.append(createDog(createNewPetElement(name),name,gender,breed, img));
-									}
-								}
-							}
-						}
-						function comparisons(name,names){
-							var x = 0;
-							var i;
-							for (i = 0; i < names.length; i++) {
-								if (name.toLowerCase() === names[i].toLowerCase()){
-									x = 1;
-								}
-							}
-							return x;
-						}
-						function doesFileExist(urlToFile) {
-							var xhr = new XMLHttpRequest();
-							xhr.open('HEAD', urlToFile, false);
-							xhr.send();
-							if (xhr.status == "404") {
-								return false;
-							} else {
-								return true;
-							}
-						}
-						function createNewPetElement(name){
-							var name = document.createElement('div');
-							name.classList.add('pet');
-							return name;
-						}
-						function createPetTitle(name){
-							let pet__title = document.createElement('h2');
-							pet__title.textContent = name;
-							pet__title.classList.add('pet__title');
-							return pet__title;
-						}
-						function createPetInformation(gender,breed){
-							let pet__information_div = document.createElement('div');
-							let pet__gender = document.createElement('p');
-							let pet__breed = document.createElement('p');
-							
-							pet__information_div.classList.add('pet__information');
-							pet__gender.textContent = gender;
-							pet__breed.textContent = breed;
-							pet__information_div.append(pet__gender,pet__breed);
-							return pet__information_div;
-						}
-						function createPetImage(img){
-							let pet__img = document.createElement('img');
-							
-							pet__img.setAttribute("src",img);
-							pet__img.setAttribute("width","100%");
-							pet__img.setAttribute("height","100%");
-							pet__img.setAttribute("alt", "Pet picture");
-							return pet__img;
-						}
-						function createPetButtons(){
-							let button_div = document.createElement('div');
-							let button_wishList = document.createElement('a');
-							let button_viewProfile = document.createElement('a');
-							
-							button_div.classList.add('btn__wrapper');
-							
-							button_wishList.classList.add('btn');
-							button_wishList.classList.add('btn-color');
-							button_wishList.textContent = 'Add to Wish List';
-							
-							button_viewProfile.classList.add('btn');
-							button_viewProfile.classList.add('btn-color');
-							button_viewProfile.textContent = "View Pet Profile";
-							
-							button_wishList.addEventListener('click',function(){
-								console.log('you click on wish list for dog');
-							});
-							button_viewProfile.addEventListener('click',function(){
-								console.log('you click on view pet profile for dog');
-							});
-							
-							button_div.append(button_wishList,button_viewProfile);
-							return button_div;
-						}
-						function createPet(varname, name, gender, breed, img){
-							varname.setAttribute('id','dog'+(++dogCounter));
-							varname.style.marginLeft = "1.2em";
-							varname.append(createPetTitle(name),createPetImage(img),createPetInformation(gender,breed),createPetButtons());
-							return varname;
-						}
-					</script>
-
+<?php $name = $_POST["name"]; ?>
+<?php $gender = $_POST["gender"]; ?>
+<?php $dog = $_POST["dog"]; ?>
+<script>
+var name = "<?php echo $name;?>";
+var gender = "<?php echo $gender;?>";
+var breed = "<?php echo $dog;?>";
+var Dogs = document.getElementById("dogs");
+var names = ["Bella","Charlie","Coco","Daisy","Lola","Molly","Phoebe","Poppy","Rosie","Teddy"];
+var breeds = ["American Pit Bull Terrier","American Staffordshire Terrier","Australian Cattle Dog","Australian Shepherd","Border Collie","Boxer","Dachshund","German Shepherd","Rottweiler","Shih Tzu"];
+var dogs = document.getElementById("dogs");
+if (name.length == 0 || comparisons(name,names) == false){
+	if (gender.length == 0){
+		if (breed.length == 0 || comparisons(breed,breeds) == false){
+			var i;
+			for (i = 0; i < names.length; i++){
+				var j;
+				for (j = 0; j < breeds.length; j++){
+					createPet(names[i],"Female",breeds[j]);
+					createPet(names[i],"Male",breeds[j]);
+				}
+			}
+		}
+		else {
+			var i;
+			for (i = 0; i < names.length; i++){
+				createPet(names[i],"Female",breed);
+				createPet(names[i],"Male",breed);
+			}
+		}
+	}
+	else{
+		if (breed.length == 0 || comparisons(breed,breeds) == false){
+			var i;
+			for (i = 0; i < names.length; i++){
+				var j;
+				for (j = 0; j < breeds.length; j++){
+					createPet(names[i],gender,breeds[j]);
+				}
+			}
+		}
+		else {
+			var i;
+			for (i = 0; i < names.length; i++){
+				createPet(names[i],gender,breed);
+			}
+		}
+	}
+}
+else{
+	if (gender.length == 0){
+		if (breed.length == 0 || comparisons(breed,breeds) == false){
+			var j;
+			for (j = 0; j < breeds.length; j++){
+				createPet(name,"Female",breeds[j]);
+				createPet(name,"Male",breeds[j]);
+			}
+		}
+		else {
+			createPet(name,"Female",breed);
+			createPet(name,"Male",breed);
+		}
+	}
+	else{
+		if (breed.length == 0 || comparisons(breed,breeds) == false){
+			var j;
+			for (j = 0; j < breeds.length; j++){
+				createPet(name,gender,breeds[j]);
+			}
+		}
+		else {
+			createPet(name,gender,breed);
+		}
+	}
+}
+function createPet(name,gender,breed){
+	var img = "../Web_Dev/pet_images/" + name + "/" + gender + "/" + breed + "/" + name + ".jpg\n";
+	if (doesFileExist(img) == true){
+		var d = Dogs.appendChild(createNewPetElement(name));
+		d.appendChild(createPetTitle(name));
+		d.appendChild(createPetImage(img));
+		d.appendChild(createPetInformation(gender,breed));
+		d.appendChild(createPetButtons());
+	}
+}
+function createNewPetElement(name){
+	var name = document.createElement('div');
+	name.classList.add('pet');
+	return name;
+}
+function createPetTitle(name){
+	let pet__title = document.createElement('h2');
+	pet__title.textContent = name;
+	pet__title.classList.add('pet__title');
+	return pet__title;
+}
+function createPetImage(img){
+	let pet__img = document.createElement('img');
+	pet__img.setAttribute("src",img);
+	pet__img.setAttribute("width","100%");
+	pet__img.setAttribute("height","100%");
+	pet__img.setAttribute("alt", "Pet picture");
+	return pet__img;
+}
+function createPetInformation(gender,breed){
+	var pet__information_div = document.createElement('div');
+	pet__information_div.classList.add('pet__information');
+	let pet__gender = document.createElement('p');
+	pet__gender.textContent = gender;
+	pet__information_div.appendChild(pet__gender);
+	let pet__breed = document.createElement('p');
+	pet__breed.textContent = breed;
+	pet__information_div.appendChild(pet__breed);
+	return pet__information_div;
+}
+function createPetButtons(){
+	let button_div = document.createElement('div');
+	let button_wishList = document.createElement('a');
+	let button_viewProfile = document.createElement('a');
+	button_div.classList.add('btn__wrapper');
+	button_wishList.classList.add('btn');
+	button_wishList.classList.add('btn-color');
+	button_wishList.textContent = 'Add to Wish List';
+	button_viewProfile.classList.add('btn');
+	button_viewProfile.classList.add('btn-color');
+	button_viewProfile.textContent = "View Pet Profile";
+	button_wishList.addEventListener('click',function(){
+		console.log('you click on wish list for dog');
+	});
+	button_viewProfile.addEventListener('click',function(){
+		console.log('you click on view pet profile for dog');
+	});
+	button_div.append(button_wishList,button_viewProfile);
+	return button_div;
+}
+function doesFileExist(urlToFile) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('HEAD', urlToFile, false);
+	xhr.send();
+	if (xhr.status == "404") {
+		return false;
+	} else {
+		return true;
+	}
+}
+function comparisons(name,names){
+	var x = false;
+	var i;
+	for (i = 0; i < names.length; i++) {
+		if (name.toLowerCase() === names[i].toLowerCase()){
+			x = true;
+		}
+	}
+	return x;
+}
+</script>
                 </section>
-
             </section> <!--end section for pet display-->
-            
         </main>
 
         <footer>
